d0e8fce4cb8fe44ce04e133ebca6bc3d4a6d628d
viswapramod70 <k.viswapramod@gmail.com>
2025-10-28 15:20:43 +0530

    Time: 1 ms (31.92%), Space: 45.7 MB (13.83%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,13 +1,38 @@
 class Solution {
-    public int missingNumber(int[] nums) {
-        int xor1=0;
-		int xor2=0;
-		for(int i=0;i<=nums.length;i++) {
-			xor1^=i;
+    // public int missingNumber(int[] nums) {
+    //     int xor1=0;
+	// 	int xor2=0;
+	// 	for(int i=0;i<=nums.length;i++) {
+	// 		xor1^=i;
+	// 	}
+	// 	for (int num : nums) {
+	// 		xor2^=num;
+	// 	}
+	// 	return xor1^xor2;
+    // }
+    public static int missingNumber(int[] arr) {
+        //using cyclic sort
+		int i=0;
+		while(i<arr.length) {
+			int correct = arr[i];
+			if(arr[i]<arr.length && arr[i]!=arr[correct]){
+				swap(arr,i,correct);
+            }
+			else{
+                i++;
+            }
 		}
-		for (int num : nums) {
-			xor2^=num;
+		// Search for 1st missing number
+		for(int index=0;index<arr.length;index++) {
+			if(arr[index]!=index)
+				return index;
 		}
-		return xor1^xor2;
+		// case 2
+		return arr.length;
     }
+    public static void swap(int[] arr,int first,int second) {
+		int temp = arr[first];
+		arr[first] = arr[second];
+		arr[second] = temp;
+	}
 }
