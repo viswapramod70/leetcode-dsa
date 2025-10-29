561db30c19aac618fdf1e8637adbdc71ad6192e1
viswapramod70 <k.viswapramod@gmail.com>
2025-10-29 11:19:48 +0530

    Time: 6 ms (50.49%), Space: 58.6 MB (23.19%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,17 +1,42 @@
 class Solution {
-    public int findDuplicate(int[] nums) {
-        int hare=0,tortoise=0;
-        do
-        {
-            tortoise=nums[tortoise];
-            hare=nums[nums[hare]];
-        }while(tortoise!=hare);
-        tortoise=0;
-        while(tortoise!=hare)
-        {
-            tortoise=nums[tortoise];
-            hare=nums[hare];
+    // public int findDuplicate(int[] nums) {
+    //     int hare=0,tortoise=0;
+    //     do
+    //     {
+    //         tortoise=nums[tortoise];
+    //         hare=nums[nums[hare]];
+    //     }while(tortoise!=hare);
+    //     tortoise=0;
+    //     while(tortoise!=hare)
+    //     {
+    //         tortoise=nums[tortoise];
+    //         hare=nums[hare];
+    //     }
+    //     return tortoise;
+    // }
+    public int findDuplicate(int[] nums){
+        //cyclic sort
+        int i=0, count = 0;
+        while(i<nums.length){
+            if(nums[i] != i+1){
+                int correct_index = nums[i]-1;
+                if(nums[i]!=nums[correct_index])
+                    swap(nums,i,correct_index);
+                else
+                    return nums[i];
+            }
+            else
+                i++;
         }
-        return tortoise;
+        for(int j=0;j<nums.length;j++){
+            if(nums[j]!=j+1)
+                count++;
+        }
+        return count;
+    }
+    public void swap(int[] arr,int first, int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
     }
 }
