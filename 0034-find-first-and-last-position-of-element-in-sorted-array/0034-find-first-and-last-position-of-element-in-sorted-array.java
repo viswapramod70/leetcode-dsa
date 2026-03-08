ca6e3db50a6813f73c62eeae1c8f3a5a6c7058cf
viswapramod70 <k.viswapramod@gmail.com>
2026-03-08 14:33:57 +0530

    Time: 0 ms (100%), Space: 48.1 MB (69.65%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,29 +1,27 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
         int[] ans = {-1,-1};
-        int start = search(nums,target,true);
-        int end = search(nums,target,false);
-        ans[0] = start;
-        ans[1] = end;
+        ans[0] = search(nums,target,true);  //To find the 1st occurence
+        if(ans[0] != -1)
+            ans[1] = search(nums,target,false);
         return ans;
     }
-
-    int search(int[] nums,int target, boolean findStartIndex){
+    public int search(int[] nums,int target,boolean findStartingIndex){
         int ans = -1;
-        int start = 0, end = nums.length-1;
-        while(start<=end){
-            int mid = start + (end-start)/2;
-            if(target<nums[mid])
-                end = mid-1;
-            else if(target>nums[mid])
-                start = mid+1;
+        int s = 0,e = nums.length-1;
+        while(s<=e){
+            int mid = s + (e-s)/2;
+            if(target > nums[mid])
+                s = mid + 1;
+            else if(target < nums[mid])
+                e = mid - 1;
             else{
                 ans = mid;
-                if(findStartIndex)
-                    end = mid-1;
+                if(findStartingIndex)
+                    e = mid - 1;
                 else
-                    start = mid+1;
-            }  
+                    s = mid + 1; 
+            }
         }
         return ans;
     }
